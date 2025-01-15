93cfc6fbfe0ca31476740dbf57d4b6bb3df1375c
shinyaurora <stuartevans.top@gmail.com>
2025-01-15 04:49:20 -0800

    add: theme provider component add

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/agents/:id" element={<h1>Detail Page</h1>}></Route>
-          <Route path="*" element={<h1>Not Found</h1>} />
-        </Routes>
-      </Router>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Router>
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/agents/:id" element={<h1>Detail Page</h1>}></Route>
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </Provider>
   )
 }
